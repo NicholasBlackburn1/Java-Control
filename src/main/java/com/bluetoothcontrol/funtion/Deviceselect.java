af26d8f813f69733f61bf94571bcc861f74263ec
NICKY B <Nickblackburn02@gmail.com>
2019-02-22 07:41:49 -0500

    added server

--- src/main/java/com/bluetoothcontrol/funtion/Deviceselect.java
+++
@@ -1,9 +0,0 @@
-package com.bluetoothcontrol.funtion;
-
-public class Deviceselect{
-    public static void select(){
-    
-    
-
-    }
-}
--- src/main/java/com/bluetoothcontrol/main.java
+++ src/main/java/com/bluetoothcontrol/main.java
@@ -2,7 +2,6 @@ package com.bluetoothcontrol;
 import com.bluetoothcontrol.funtion.*;
 public class main {
 public static void main(String[] args){
-
-    Searchclient.BlueSearch();
+MyDiscoveryListener.run();
 }
 }
