cdfa3ea01c8a66c892be48c2c3a8897e872cbc61
nicholasblackburn1 <nickblackburn02@gmail.com>
2019-02-28 10:25:19 -0500

    tring to add scaneer

--- src/main/java/com/bluetoothcontrol/Main.java
+++ src/main/java/com/bluetoothcontrol/Main.java
@@ -1,32 +1,9 @@
 package com.bluetoothcontrol;
 
-import java.awt.Window;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.InputMismatchException;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
-import java.util.Timer;
-
-import javax.swing.plaf.TextUI;
-
-import com.bluetoothcontrol.Textui.Textui;
-import com.bluetoothcontrol.clidata.pos;
-import com.bluetoothcontrol.client.Mqttclient;
-import com.bluetoothcontrol.client.Tcpclient;
-import com.bluetoothcontrol.server.BluetoothServer;
-import com.bluetoothcontrol.server.Tcpserver;
 
 public class Main {
-    // reads user input to do an action from a number list 
-    public static void main(String[] args) throws NoSuchElementException {
-        Scanner keyboard = new Scanner(System.in);
-        int myint = keyboard.nextInt();
-         System.out.println("enter an integer"); 
-         
-         if(myint == 1){
-             System.out.println("hello");
-         }
+    // reads user input to do an action from a number list
+    public static void main(String[] args) {
 
     }
 }
---
+++ src/main/java/com/bluetoothcontrol/client/KeyScanner.java
@@ -0,0 +1,17 @@
+package com.bluetoothcontrol.client;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class KeyScanner{
+    public static void Keybord(){
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //Here you declare your BufferedReader object and instance it.
+        Scanner keyboard = new Scanner(System.in);
+         int menu = keyboard.nextInt();
+         //We read from user's input
+            
+        }
+        
+    }
