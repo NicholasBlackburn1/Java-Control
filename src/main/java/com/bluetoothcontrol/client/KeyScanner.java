53a76b4aab92f5f2205c47d90776c16d9f222f79
nicholasblackburn1 <nickblackburn02@gmail.com>
2019-02-28 11:07:28 -0500

    addeding buffered rerader

--- src/main/java/com/bluetoothcontrol/client/KeyScanner.java
+++ src/main/java/com/bluetoothcontrol/client/KeyScanner.java
@@ -1,17 +1,21 @@
 package com.bluetoothcontrol.client;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class KeyScanner{
     public static int num2 = 2;
     public static int num1 = 1;
-    public static void Keybord(){
+    public static void Keybord() throws IOException{
 
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //Here you declare your BufferedReader object and instance it.
-        Scanner keyboard = new Scanner(System.in);
-         int menu = keyboard.nextInt();
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+
+    System.out.println("tets");
+    //Here you declare your BufferedReader object and instance it.
+    while (true){
+         int menu = br.read();
          //We read from user's input
         
          if (menu == num1){
@@ -23,3 +27,4 @@ public class KeyScanner{
         }
         
     }
+}
