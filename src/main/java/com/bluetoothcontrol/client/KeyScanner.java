1013f451a13406d13781305f8bae93cf9de8c3af
nicholasblackburn1 <nickblackburn02@gmail.com>
2019-02-28 10:30:09 -0500

    adding  key scanner for menu

--- src/main/java/com/bluetoothcontrol/client/KeyScanner.java
+++ src/main/java/com/bluetoothcontrol/client/KeyScanner.java
@@ -5,12 +5,21 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class KeyScanner{
+    public static int num = 0;
     public static void Keybord(){
 
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //Here you declare your BufferedReader object and instance it.
         Scanner keyboard = new Scanner(System.in);
          int menu = keyboard.nextInt();
          //We read from user's input
+         switch () {
+             case value:
+                 
+                 break;
+         
+             default:
+                 break;
+         }
             
         }
         
